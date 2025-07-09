47aff4ab00bb45719f33eac017e8885e4e60a8fe
ayanakoji@DESKTOP-NOC7NTA <olamiposiomisade9@gmail.com>
2025-07-09 01:49:13 +0100

    Add broadcast artifacts and fix Interactions script

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -54,9 +54,7 @@ contract FundMe {
             s_addressToAmountFunded[funder] = 0;
         }
         s_funders = new address[](0);
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
 
@@ -70,9 +68,7 @@ contract FundMe {
             s_addressToAmountFunded[funder] = 0;
         }
         s_funders = new address[](0);
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
+       (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
     // Explainer from: https://solidity-by-example.org/fallback/
